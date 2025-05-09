93eb769d41a10d56a980ac8a66ea17da26ba2ff5
alex9043 <alex980127@gmail.com>
2025-05-09 13:59:35 +0300

    Init project

---
+++ src/main/java/ru/alex9043/wrtesttask/WrTestTaskApplication.java
@@ -0,0 +1,13 @@
+package ru.alex9043.wrtesttask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WrTestTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WrTestTaskApplication.class, args);
+    }
+
+}
